f89d2d9fd3b865fb83d1e03237599e2ee169f625
BALAKUMAR P <80500214+BK3820@users.noreply.github.com>
2021-07-13 06:42:29 +0530

    Create Maximum_money.c

---
+++ Maximum_money.c
@@ -0,0 +1,17 @@
+
+ int n,m,test;
+ scanf("%d",&test);
+ while(test--)
+ {
+  scanf("%d",&n);
+  scanf("%d",&m);
+ if(n%2==0)
+ {printf("%d\n",(n/2)*m);
+ }
+ else
+ {printf("%d\n",((n/2)+1)*m);
+ }
+ }
+return 0;
+
+}
