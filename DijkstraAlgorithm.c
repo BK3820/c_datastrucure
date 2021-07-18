771f195c3a199a548ec5eb1a3c42ec2063462748
BALAKUMAR P <80500214+BK3820@users.noreply.github.com>
2021-07-18 07:08:14 +0530

    Update DijkstraAlgorithm.c

--- DijkstraAlgorithm.c
+++ DijkstraAlgorithm.c
@@ -8,3 +8,9 @@ void Dijkstra(int Graph[MAX][MAX], int n, int start);
 void Dijkstra(int Graph[MAX][MAX], int n, int start) {
   int cost[MAX][MAX], distance[MAX], pred[MAX];
   int visited[MAX], count, mindistance, nextnode, i, j;
+for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      if (Graph[i][j] == 0)
+        cost[i][j] = INFINITY;
+      else
+        cost[i][j] = Graph[i][j];
