2589c3c34164aaf88e5e1da097dbe7ff8e1dc1a8
BALAKUMAR P <80500214+BK3820@users.noreply.github.com>
2021-07-18 07:07:50 +0530

    Create DijkstraAlgorithm.c

---
+++ DijkstraAlgorithm.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#define INFINITY 9999
+#define MAX 10
+
+void Dijkstra(int Graph[MAX][MAX], int n, int start);
+
+void Dijkstra(int Graph[MAX][MAX], int n, int start) {
+  int cost[MAX][MAX], distance[MAX], pred[MAX];
+  int visited[MAX], count, mindistance, nextnode, i, j;
